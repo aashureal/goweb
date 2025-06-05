984ea341f61a8539753ab15257c6d69f9cf9662d
Aashutosh Uikey <aashureal01@gmail.com>
2025-06-05 11:38:34 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	app := gin.Default()
+
+	// Load templates
+	app.LoadHTMLGlob("templates/*")
+
+	app.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{
+			"Title":   "Welcome",
+			"Message": "Welcome to GoWeb",
+
+		})
+	})
+
+	app.Run((":8080"))
+
+}
